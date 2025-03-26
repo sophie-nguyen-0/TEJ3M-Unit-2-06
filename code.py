dfcb6e5c1d03b2989a1152723823961e62880422
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2025-03-26 09:51:20 -0400

    py code

--- code.py
+++ code.py
@@ -5,3 +5,23 @@ by: Sophie Nguyen
 using sonar with arduino
 
 """
+
+import time
+import board
+import adafruit_hcsr04
+
+#constants
+led = digitalio.DigitalInOut(board.GP5)
+led.direction = digitalio.Direction.OUTPUT
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP2, echo_pin=board.GP3)
+
+led.value = False
+
+while True:
+    if sonar.distance < 10:
+         #turns on LED
+        led.value = True
+
+    else:
+        #turns LED off
+        led.value = False
