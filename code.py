d1c033ce35e5478a7256f62f7a2b2bf6b92e43ec
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2025-04-02 09:28:33 -0400

    py code

--- code.py
+++ code.py
@@ -5,23 +5,38 @@ by: Sophie Nguyen
 using sonar with arduino
 
 """
-
 import time
 import board
 import adafruit_hcsr04
+import digitalio
 
-#constants
-led = digitalio.DigitalInOut(board.LED)
+
+# variables
+seconds_to_microseconds_conversion_number = 1000000
+sonar_delays = [2 / seconds_to_microseconds_conversion_number, 10 / seconds_to_microseconds_conversion_number]
+delay_between_sonar_cheeks = 10
+distance = 0
+too_close = 20
+
+# setup
+led = digitalio.DigitalInOut(board.GP12)
 led.direction = digitalio.Direction.OUTPUT
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP2, echo_pin=board.GP3)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin = board.GP15, echo_pin = board.GP14)
 
-led.value = False
-
+# loop
 while True:
-    if sonar.distance < 10:
-         #turns on LED
-        led.value = True
+    # Sonar gets the distance form object
+    time.sleep(sonar_delays[0])
+    distance = sonar.distance
+    time.sleep(sonar_delays[1])
 
+    print(f"Distance: {distance} cm")
+
+    # Turns on LED if an object’s distance is equal to or closer then 20 cm from the sonar
+    if distance <= too_close0:
+        led.value = True
     else:
-        #turns LED off
         led.value = False
+
+    # The commented out code is not part of the actual code but is needed to get it working by uncommenting it and then recommenting it
+    #time.sleep(delay_between_sonar_cheeks)
